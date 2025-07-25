17de3415b9999e0662654f46b9b9640ce2dafb80
AlaiWebDev <alainwebdev@free.fr>
2025-07-25 11:36:07 +0200

    END suppression-index

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,12 @@ import Card from '../components/Card'
 import DatasService from '../services/DatasService'
 function Home({auteur}) {
   const realisations = DatasService()
-
   return (
     <div className="w-2/3 p-4 m-auto">
       <h1 className="text-2xl font-bold mb-4">Bienvenue sur cette démonstration</h1>
       <h2>Développé par {auteur}</h2>
       <section className='flex flex-row flex-wrap justify-center gap-8 my-8'>
-        {realisations.projects.map((project, index) => (
+        {realisations.projects.map((project) => (
           <Card real={project} />
         ))}
       </section>
